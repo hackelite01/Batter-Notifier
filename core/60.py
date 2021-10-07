8a53ffab0b668ffe6c74287f9513e807c608f4e2
HackElite <78653024+hackelite01@users.noreply.github.com>
2021-10-07 22:01:45 +0530

    Batter-Notifier

---
+++ core/60.py
@@ -0,0 +1,55 @@
+# pip install psutil
+import psutil
+
+battery = psutil.sensors_battery()
+plugged = battery.power_plugged
+percent = battery.percent
+
+if percent >= # pip install psutil
+import psutil
+
+battery = psutil.sensors_battery()
+plugged = battery.power_plugged
+percent = battery.percent
+
+if percent >= 30:
+ 
+    # pip install py-notifier
+    # pip install win10toast
+    from pynotifier import Notification
+
+    Notification(
+        title="Battery Low",
+        description=str(percent) + "% Battery remain!!",
+        duration=5,  # Duration in seconds
+        urgency=Notification.URGENCY_CRITICAL,
+    ).send()6# pip install psutil
+import psutil
+
+battery = psutil.sensors_battery()
+plugged = battery.power_plugged
+percent = battery.percent
+
+if percent >= 60:
+ 
+    # pip install py-notifier
+    # pip install win10toast
+    from pynotifier import Notification
+
+    Notification(
+        title="Battery Low",
+        description=str(percent) + "% Battery remain!!",
+        duration=5,  # Duration in seconds
+        urgency=Notification.URGENCY_CRITICAL,
+    ).send():
+ 
+    # pip install py-notifier
+    # pip install win10toast
+    from pynotifier import Notification
+
+    Notification(
+        title="Battery Low",
+        description=str(percent) + "% Battery remain!!",
+        duration=5,  # Duration in seconds
+        urgency=Notification.URGENCY_CRITICAL,
+    ).send()
